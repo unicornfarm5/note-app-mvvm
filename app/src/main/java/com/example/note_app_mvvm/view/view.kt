4ace00f160c97712e9cedda296d3b021c317c41b
Christian-Skovgaard <cellochr@gmail.com>
2025-03-17 10:56:57 +0100

    it works

--- app/src/main/java/com/example/note_app_mvvm/view/view.kt
+++ app/src/main/java/com/example/note_app_mvvm/view/view.kt
@@ -1,4 +1,55 @@
 package com.example.note_app_mvvm.view
 
-class view {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import com.example.note_app_mvvm.viewmodel.TitleCard
+
+@Composable
+fun TitleCard (TitleCard: TitleCard) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        Text(text = "Todo app", fontSize = 32.sp)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Title input
+        TextField(
+            value = titleText,
+            label = { Text("Title") },
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { titleText = it }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Description input
+        TextField(
+            value = descriptionText,
+            label = { Text(viewModel.titletext) },
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { descriptionText = it }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Add Todo Button
+        Button(onClick = {
+            if (titleText.isNotBlank()) {
+                todos.add(TodoItem(titleText, descriptionText))
+                titleText = ""
+                descriptionText = ""
+            }
+        }) {
+            Text("Add todo")
+        }
 }
--- app/src/main/java/com/example/note_app_mvvm/viewmodel/viewmodel.kt
+++ app/src/main/java/com/example/note_app_mvvm/viewmodel/viewmodel.kt
@@ -1,4 +1,15 @@
 package com.example.note_app_mvvm.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
 class viewmodel {
+}
+
+class TitleCard:ViewModel () {
+    var titleText = { mutableStateOf("") }
+    var descriptionText = { mutableStateOf("") }
 }
